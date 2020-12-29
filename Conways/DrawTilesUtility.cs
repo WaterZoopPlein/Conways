4db64f7894c0f7a4bf79b38fa5cca54f5648709a
WaterZoopPlein <28999049+WaterZoopPlein@users.noreply.github.com>
2020-12-29 18:44:59 +0000

    mouse + keyboard inputs

---
+++ Conways/DrawTilesUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Conways
+{
+    public class DrawTilesUtility
+    {
+        public static List<List<Color>> GenerateRandomTiles(GraphicsDeviceManager graphics, Color[] tileColors)
+        {
+            var tileColorList = new List<List<Color>>();
+
+            var rand = new Random();
+
+            for (var i = 0; i < graphics.PreferredBackBufferHeight / 20; i++)
+            {
+                tileColorList.Add(new List<Color>());
+                for (var j = 0; j < graphics.PreferredBackBufferWidth / 20; j++)
+                {
+                    var colorsIndex = rand.Next(tileColors.Length);
+                    tileColorList[i].Add(tileColors[colorsIndex]);
+                }
+            }
+
+            return tileColorList;
+        }
+
+        public static List<List<Color>> ResetTiles(GraphicsDeviceManager graphics)
+        {
+            var tileColorList = new List<List<Color>>();
+
+            for (var i = 0; i < graphics.PreferredBackBufferHeight / 20; i++)
+            {
+                tileColorList.Add(new List<Color>());
+                for (var j = 0; j < graphics.PreferredBackBufferWidth / 20; j++)
+                {
+                    tileColorList[i].Add(new Color(64, 64, 64));
+                }
+            }
+
+            return tileColorList;
+        }
+
+        public static List<List<Color>> Checkerboard(GraphicsDeviceManager graphics)
+        {
+            var tileColorList = new List<List<Color>>();
+
+            for (var i = 0; i < graphics.PreferredBackBufferHeight / 20; i++)
+            {
+                tileColorList.Add(new List<Color>());
+                for (var j = 0; j < graphics.PreferredBackBufferWidth / 20; j++)
+                {
+                    tileColorList[i].Add((i + j) % 2 == 0 ? Color.Lime : new Color(64, 64, 64));
+                }
+            }
+
+            return tileColorList;
+        }
+    }
+}
